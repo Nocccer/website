f4b6b972a392b087bb9658fc143ddfa5396e35e4
Nocccer <Nocccer@gmail.com>
2021-12-27 10:56:07 +0100

    Added meta data by hand

--- src/screens/NewRelease.tsx
+++ src/screens/NewRelease.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ReleaseConf } from '../data/Releases';
 import { OffCanvas } from '../component/OffCanvas';
-import {Helmet} from "react-helmet";
 
 export function NewRelease({
     name,
@@ -15,15 +14,6 @@ export function NewRelease({
 {
     return (
         <>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <meta property="og:title" content={`${name} out now!`} />
-                <meta property="og:type" content="music" />
-                <meta property="og:url" content={`https://www.weareisolate.com/#/${name.toLowerCase()}`} />
-                <meta property="og:image" content={picture} />
-                <title>{name} out now!</title>
-                <link rel="canonical" href={`https://www.weareisolate.com/#/${name.toLowerCase()}`} />
-            </Helmet>
             <OffCanvas name={name} show={true} streams={streams} buys={buys} renderCloseButton={false} image={picture} colors={colorStyle}/>
         </>
     );
